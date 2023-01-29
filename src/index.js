844e476d9b7fa80c5dc02efd87e84b693e399b67
Victor Monje <monjev086@gmail.com>
2023-01-28 18:07:38 -0600

    hook - useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom/client";
 //import { Greeting, UserCard } from "./Greeting";
 //import Product, { Nanbar } from "./Product";
@@ -30,23 +30,24 @@ const root = ReactDom.createRoot(document.getElementById("root"));
 function Counter() {
   // var and function = useState
   const [message, setMessage] = useState("Usuario");
-  //use state container of values and return var and func change value of var
+  const [counter, setCounter] = useState(0);
+  //use state container of values and return var and a func change value of var
+
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
 
   return (
     <div>
-      <input
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          alert("the Message is : " + message);
-        }}
-      >
-        Save
-      </button>
+      <input onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={() => alert("the Message is : " + message)}>Save</button>
       <h1>{message}</h1>
+
+      <hr />
+
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
     </div>
   );
 }
